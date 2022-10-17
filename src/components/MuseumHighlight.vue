76149e9d80da9b184c8974d1ebaaac1560702d0e
Zak Hooper <zakhooper93@Gmail.com>
2022-10-17 13:02:48 +0100

    feat: MVP

---
+++ src/components/MuseumHighlight.vue
@@ -0,0 +1,104 @@
+<template>
+    <h2 class="blue-key">Core museum highlights</h2>
+    <h2 class="orange-key">From our partners</h2>
+    <div class="museum-highlight-grid">
+        <li v-for="item in highlights" :key="item">
+            <img
+                :src="require(`/src/assets/highlights/${this.highlightImage(item.image)}.jpg`)"
+                :class="imageOutlineColour(item.id)"
+            />
+            <img :src="require(`../assets/icons/${pageTheme}.png`)" class="theme-icon" />
+            <h2 class="highlight-title">{{ item.name }}</h2>
+            <div class="highlight-text">
+                <p v-if="item.description">{{ item.description }}</p>
+                <p v-if="item.info">{{ item.info }}</p>
+                <p v-if="item.news?.title">{{ item.news.title }}</p>
+                <p v-if="item.news?.date">{{ item.news.date }}</p>
+                <a :href="item.quiz" v-if="item.quiz">Take our related quiz!</a>
+            </div>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MuseumHighlight',
+    props: {
+        highlights: {
+            type: Object
+        },
+        pageTheme: {
+            type: String
+        }
+    },
+    methods: {
+        highlightImage(highlightImage) {
+            return highlightImage ? highlightImage : `${this.pageTheme}Placeholder`;
+        },
+        imageOutlineColour(itemId) {
+            return itemId ? 'highlight-image-museum' : 'highlight-image-partner';
+        },
+        themeIcon(theme) {
+            return `../assets/icons/${theme}.jpg`;
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.museum-highlight-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+.museum-highlight-grid li {
+    width: 1fr;
+    height: 1fr;
+    margin: 50px;
+    list-style: none;
+}
+
+.highlight-image-museum, .highlight-image-partner {
+    width: 400px;
+    height: 400px;
+
+}
+
+.highlight-image-museum {
+    box-shadow: 5px 5px 10px blue;
+}
+
+.highlight-image-partner {
+    box-shadow: 5px 5px 10px orange;
+}
+
+.highlight-title {
+    text-decoration: underline;
+}
+
+.highlight-text {
+    text-align: left;
+    margin-bottom: 5%;
+}
+
+.theme-icon {
+    bottom: 100%;
+    left: 0%;
+    width: 50px;
+    height: 50px;
+    margin: 0;
+}
+.blue-key,
+.orange-key {
+    font-size: 12px;
+    font-weight: 1000;
+    text-align: left;
+    margin-left: 5%;
+}
+.blue-key {
+    color: blue;
+}
+.orange-key {
+    color: orange;
+}
+</style>
---
+++ src/components/SpacePage.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="space-page">
+        <h1 class="space-page__title">Space</h1>
+        <museum-highlight 
+        :highlights="dateSortedSpaceHighlights" 
+        pageTheme="space" 
+        />
+    </div>
+</template>
+
+<script>
+import _ from 'lodash';
+import MuseumHighlight from './MuseumHighlight.vue';
+
+export default {
+    name: 'SpacePage',
+    components: {
+        MuseumHighlight
+    },
+
+    data() {
+        return {
+            spaceHighlights: [
+                {
+                    date: '2020-04-20 12:20:00',
+                    description:
+                        'Asteroids are minor planets, especially of the inner Solar System. Larger asteroids have also been called planetoids.',
+                    id: 1,
+                    image: '',
+                    name: 'Asteroids'
+                },
+                {
+                    date: '2020-05-20 15:50:00',
+                    description:
+                        'A comet is an icy, small Solar System body that, when passing close to the Sun, warms and begins to release gases, a process called outgassing.',
+                    id: 9,
+                    image: '',
+                    name: 'Comets'
+                },
+                {
+                    date: '2020-05-01 9:22:00',
+                    description:
+                        'The term planet is ancient, with ties to history, astrology, science, mythology, and religion.',
+                    id: 7,
+                    image: '',
+                    name: 'Planets',
+                    news: {
+                        date: '2020-08-18 18:00:00',
+                        title: 'Attend our talk about Jupiter with Dr. Hogarth'
+                    },
+                    quiz: 'https://planetquiz.space'
+                },
+                {
+                    date: '2020-07-05 4:10:00',
+                    description:
+                        'A meteor shower is a celestial event in which a number of meteors are observed to radiate, or originate, from one point in the night sky.',
+                    id: 12,
+                    image: '',
+                    name: 'Meteor showers',
+                    news: {
+                        title: 'The Lyrids will peak at on April 21-22 2021, at night'
+                    }
+                }
+            ],
+            spacePartners: {
+                observatory: {
+                    createdAt: '2020-06-01 11:45:00',
+                    info: 'The Mauna Kea Observatories (MKO) are a number of independent astronomical research facilities and large telescope observatories that are located at the summit of Mauna Kea on the Big Island of Hawaiʻi, United States.',
+                    image: '',
+                    name: 'Mauna Kea Observatories'
+                }
+            }
+        };
+    },
+
+    computed: {
+        dateSortedSpaceHighlights() {
+            return _.sortBy(this.mergedHighlights(), [
+                function (o) {
+                    return o.user;
+                }
+            ]).reverse();
+        }
+    },
+
+    methods: {
+        mergedHighlights() {
+            return new Array(...this.spaceHighlights, ...Object.values(this.spacePartners))
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.space-page {
+    &__title {
+        font-family: sans-serif;
+        color: #2c3791;
+        font-size: 36px;
+        font-weight: 300;
+        text-align: left;
+        margin-left: 5%;
+        text-decoration: underline;
+    }
+}
+</style>
